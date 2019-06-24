7e2faf90a1c503fcd9451a8a9bdb7de78babdcb3
MalSemik <malg.sem@gmail.com>
2019-06-24 19:59:46 +0200

    Completed version

--- my_tictactoe.py
+++ my_tictactoe.py
@@ -1,3 +1,5 @@
+import random
+
 size = 3
 
 
@@ -7,7 +9,8 @@ def create_board(size):
 
 
 def print_board(board):
-    pass
+    for k in range(len(board)):
+        print(board[k])
 
 
 def numarate_cells():  # tu powinien przyjmować tablicę i sam se ją tak numerować, ale nie mogę wymyślic jak
@@ -20,88 +23,96 @@ def select_walid_cell(board):
         choice = int(input("Podaj komórkę"))
         i = (choice - 1) // 3
         j = (choice - 1) % 3
-        print(i,j)
+        # print(i, j)
         chosen_cell = board[i][j]
         if chosen_cell == " ":
             return i, j
         else:
             print("Ta komórka jest już zajęta, spróbuj ponownie.")
 
-select_walid_cell(create_board(3))
-def select_player():  # tu coś zmieniającego z X na O... jakieś podanie imienia gracza czy cuś też by było
-    pass
-    return player_symbol
-    # można stworzyć słownik playerów albo klasę
+
+def name_players():
+    players = {}
+    players["O"] = input("Podaj imię gracza O: ")
+    players["X"] = input("Podaj imię gracza X: ")
+    return players
 
 
-def player_move(board, player_symbol):  # to też nie działa
+def select_player_who_starts(players: dict):
+    active_player = random.choice(list(players.keys()))
+    print("Zaczyna ", players[active_player])
+    return active_player
+
+
+def switch_players(active_player, players):
+    if active_player == "X":
+        active_player = "O"
+    else:
+        active_player = "X"
+    print("Ruch wykonuje", players[active_player])
+    return active_player
+
+
+def player_move(board, active_player):
     i, j = select_walid_cell(board)
-    board[i][j] = player_symbol
+    board[i][j] = active_player
+    print_board(board)
     return board
 
 
-# test_board = [[" ", " ", 3], [" ", 5, 6], [7, 8, 9]]
-# select_walid_cell(test_board, numarate_cells())
-
-test_board = [["X", "O", "X"], ["X", "X", "X"], ["X", "O", "X"]]
-
-
-def check_horizontal(board):
+def check_horizontal(board, active_player):
     for i in range(size):
-        count_of_X = board[i].count("X")
-        count_of_O = board[i].count("O")
-        if count_of_X == size:  # ten fragment też by można jakoś uniezależnić, typu "claim winner"?
-            print("Player X wins")  # tu jakieś ładne formatowanko zeby było Gosia wins, Rafał wins
-        if count_of_O == size:
-            print("Player O wins")
+        count_of_active_player = board[i].count(active_player)
+        if count_of_active_player == size:
+            return True
+    return False
 
 
-def check_vertical(board):
+def check_vertical(board, active_player):
     for i in range(size):
-        count_of_X = 0
-        count_of_O = 0
+        count_of_active_player = 0
         for j in range(len(board)):
-            if board[j][i] == "X":
-                count_of_X += 1
-            if board[j][i] == "O":
-                count_of_O += 1
-        if count_of_X == 3:  # ten fragment też by można jakoś uniezależnić, typu "claim winner"?
-            print("Player X wins")  # tu jakieś ładne formatowanko zeby było Gosia wins, Rafał wins
-        if count_of_O == 3:
-            print("Player O wins")
-        else:
-            continue
+            if board[j][i] == active_player:
+                count_of_active_player += 1
+        if count_of_active_player == size:
+            return True
+    return False
 
 
-def check_diagonal_left_right(board):
-    count_of_X = 0
-    count_of_O = 0
+def CORRECT_check_vertical(board, active_player):
     for i in range(size):
-        if board[i][i] == "X":
-            count_of_X += 1
-        if board[i][i] == "O":
-            count_of_O += 1
-    if count_of_X == 3:  # ten fragment też by można jakoś uniezależnić, typu "claim winner"?
-        print("Player X wins")  # tu jakieś ładne formatowanko zeby było Gosia wins, Rafał wins
-    if count_of_O == 3:
-        print("Player O wins")
+        count_of_active_player = 0
+        for j in range(len(board)):
+            if board[j][i] == active_player:
+                count_of_active_player += 1
+        if (
+            count_of_active_player == 3
+        ):  # ten fragment też by można jakoś uniezależnić, typu "claim winner"?
+            return True
+    return False
 
 
-def check_diagonal_right_left(board):
-    count_of_X = 0
-    count_of_O = 0
+def check_diagonal_left_right(board, active_player):
+    count_of_active_player = 0
     for i in range(size):
-        if board[i][size - i - 1] == "X":
-            count_of_X += 1
-        if board[i][size - i - 1] == "O":
-            count_of_O += 1
-    if count_of_X == 3:  # ten fragment też by można jakoś uniezależnić, typu "claim winner"?
-        print("Player X wins")  # tu jakieś ładne formatowanko zeby było Gosia wins, Rafał wins
-    if count_of_O == 3:
-        print("Player O wins")
+        if board[i][i] == active_player:
+            count_of_active_player += 1
+    if count_of_active_player == size:
+        return True
+    return False
 
 
-def check_if_full(board):
+def check_diagonal_right_left(board, active_player):
+    count_of_active_player = 0
+    for i in range(size):
+        if board[i][size - i - 1] == active_player:
+            count_of_active_player += 1
+    if count_of_active_player == size:
+        return True
+    return False
+
+
+def check_if_full(board, active_player):
     counter = 0
     for i in range(size):
         for j in range(size):
@@ -110,8 +121,34 @@ def check_if_full(board):
     if counter == size * size:
         print("Remis")
         print("Game over")
-        # play = False
+        return True
+    return False
 
 
-# check_if_full(test_board)
-check_diagonal_right_left(test_board)
+def check_all(board, active_player):
+    return (
+        check_horizontal(board, active_player)
+        or check_vertical(board, active_player)
+        or check_diagonal_right_left(board, active_player)
+        or check_diagonal_left_right(board, active_player)
+    )
+
+
+def main():
+    players = name_players()
+    active_player = select_player_who_starts(players)
+    board = create_board(3)
+    print("Wstaw symbol wybierając miejsce od 1 do 9")
+    print_board(numarate_cells())
+    while True:
+        player_move(board, active_player)
+        if check_all(board, active_player) == True:
+            print("Wygrywa", players[active_player])
+            break
+        elif check_if_full(board, active_player) == True:
+            break
+        else:
+            active_player = switch_players(active_player, players)
+
+
+main()
